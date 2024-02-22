113fafc9a7a02ba3dd6f2f529771d713ee780b61
kgatt25036 <kgatt88@gmail.com>
2024-02-22 16:46:14 +0100

    k

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,11 @@ export class AuthService {
   private isLoggedIn: boolean = false;
   private tokenKey = 'auth_token';
   private apiUrl: string = 'http://localhost:8080/auth'; // Your API URL
+  private tokenSubject: BehaviorSubject<string | null>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.tokenSubject = new BehaviorSubject<string | null>(localStorage.getItem('token'));
+  }
 
   // Method to check if user is authenticated
   isAuthenticated(): boolean {
@@ -18,22 +23,30 @@ export class AuthService {
   }
 
  
-  // Method to sign the user in
   signIn(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
+    return this.http.post<any>(`${this.apiUrl}/login`, { email, password }).pipe(
+      tap(response => {
+        const token = response.token;
+        if (token) {
+          localStorage.setItem('token', token);
+          this.tokenSubject.next(token);
+        }
+      })
+    );
   }
   storeToken(token: string): void {
     localStorage.setItem(this.tokenKey, token);
   }
 
   getToken(): string | null {
-    return localStorage.getItem(this.tokenKey);
+    return this.tokenSubject.value;
   }
 
 
   // Method to sign the user out
   signOut(): void {
-    // Implement your sign-out logic here
+    localStorage.removeItem('token');
+    this.tokenSubject.next(null);
     this.isLoggedIn = false;
   }
 }
